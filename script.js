0e32a970c5e768efff1c896ade34ed4fecf7f61f
smkh <mmm.smkh.mmm@gmail.com>
2022-08-22 23:15:01 +0430

    function toggleActiv for set active class & score

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-const pArrays=document.querySelectorAll(".get")
+const pArrays = document.querySelectorAll(".get")
+let score;
+pArrays.forEach((item) => {
+    item.addEventListener("click", () => { toggleActiv(item) })
+
+})
+const toggleActiv = (tag) => {
+    const selectedscore = document.querySelector(".active")
+    if (selectedscore === tag) {
+        selectedscore.classList.remove("active")
+        score = ""
+    } else if (selectedscore) {
+        selectedscore.classList.remove("active")
+        tag.classList.add("active")
+        score = +tag.innerHTML
+    } else {
+        tag.classList.add("active")
+        score = +tag.innerHTML
+    }
+    console.log(score)
+}
